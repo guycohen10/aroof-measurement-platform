7b25f08c92a7db9bedc5d0d20ac26d39d6053269
guy cohen <greenteamdallas@gmail.com>
2026-01-21 04:11:25 +0000

    File changes

--- src/pages/RooferDashboard.jsx
+++ src/pages/RooferDashboard.jsx
@@ -360,12 +360,12 @@ export default function RooferDashboard() {
 
         {/* Scrollable Content */}
         <div className="px-6 py-8">
-        {/* Action Center */}
-        <div className="mb-8">
-          <ActionCenter userId={user.id} companyId={user.company_id} />
-        </div>
+          {/* Action Center */}
+          <div className="mb-8">
+            <ActionCenter userId={user.id} companyId={user.company_id} />
+          </div>
 
-        {/* Company Header */}
+          {/* Company Header */}
         {user?.company_id && (
           <Card className="shadow-lg mb-8 bg-gradient-to-br from-blue-50 to-white">
             <CardContent className="p-6">
